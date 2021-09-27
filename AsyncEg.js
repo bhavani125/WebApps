31532f2082c952b9c7c154c67684d798ac04f504
bhavani125 <girinenibhavani12@gmil.com>
2021-09-27 12:12:28 +0530

    Added:code of using Async

---
+++ AsyncEg.js
@@ -0,0 +1,6 @@
+console.log("start");  // output1st start
+
+setTimeout(() =>{
+    console.log("this block is ecxuted after some time")  //3rd 
+}, 2000);
+console.log("finished")  //output2rd finished
