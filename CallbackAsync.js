d5e989909b321fc6af7c0fcdca95ae12fa837fcf
bhavani125 <girinenibhavani12@gmil.com>
2021-09-27 12:09:55 +0530

    Added:code of callback Async

---
+++ CallbackAsync.js
@@ -0,0 +1,14 @@
+// program that shows the delay in execution
+
+function fruit() {
+    console.log('apple is good ');
+
+}
+
+function chocolate(name) {
+    console.log('I want' + ' ' + name);
+}
+
+// calling the function
+setTimeout(fruit, 2000);
+chocolate('KitKat');
